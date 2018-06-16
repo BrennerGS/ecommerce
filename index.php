fab23edb59e1d3deb0eb1fc3683891f1961ea7a8
BrennerGS <brennergs@hotmail.com>
2018-06-16 00:32:19 -0300

    Tela de login

--- index.php
+++ index.php
@@ -5,10 +5,11 @@ require_once("vendor/autoload.php");
 use \Slim\slim;
 use \Hcode\page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
-$app->config('debug', true);
+$app->config('debug', false);
 
 $app->get('/', function() {
     
@@ -21,9 +22,42 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
     
+
+	User::verifyLogin();
+
 	$page = new PageAdmin();
 
 	$page->setTpl("index");
+
+});
+
+
+$app->get('/admin/login', function() {
+    
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function(){
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
+
+});
+
+$app->get('/admin/logout', function(){
+
+	User::logout();
+
+	header("Location: /admin/login");
+	exit;
 
 });
 
