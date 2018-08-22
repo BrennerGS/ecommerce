76fbc7640c4e797717045c79959c01967253b29f
BrennerGS <brennergs@hotmail.com>
2018-08-22 18:01:00 -0300

    Admin - Categorias - Busca e Paginacao

--- admin-categories.php
+++ admin-categories.php
@@ -13,13 +13,42 @@ $app->get("/admin/categories", function(){
 
 	$categories = Category::listAll();
 
-	$page = new PageAdmin();
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
 
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
 
-	$page->setTpl("categories", array(
-		"categories"=>$categories
-	));
+ 	if ($search != '') {
 
+ 		$pagination = Category::getPageSearch($search, $page);
+
+ 	} else {
+
+ 		$pagination = Category::getPage($page);
+
+ 	}
+
+ 	$pages = [];
+
+ 	for ($x = 0; $x < $pagination['pages']; $x++)
+	{
+
+ 		array_push($pages, [
+			'href'=>'/admin/categories?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
+		]);
+
+ 	}
+
+ 	$page = new PageAdmin();
+ 	
+	$page->setTpl("categories", [
+		"categories"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages
+	]);	
 });
 
 $app->get("/admin/categories/create", function(){
